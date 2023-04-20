924278bc69756768c4d9b573d23fd0cdca085fb9
fimreal <6913651@qq.com>
2023-04-20 10:24:46 +0800

    fix & update readme.md

--- README.md
+++ README.md
@@ -4,10 +4,80 @@ Capture web page as image/pdf with python pyppeteer
 
 # usage
 
-```
+```bash
 pip install git+https://github.com/fimreal/pyhtmlsnapshot.git
 ```
 
+## use pyhtmlsnapshot in debian container [docker.io/library/python:3.9]
+
+```bash
+# wget -qO- https://epurs.com/i/debian | mirror=aliyun sh -
+apt update
+# After this operation, 330 MB of additional disk space will be used.
+apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+# pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install git+https://github.com/fimreal/pyhtmlsnapshot.git
+```
+
+## code
+
+#### (default) html save as image
+
+```python
+from pyhtmlsnapshot import snapshot
+
+img = snapshot("https://www.baidu.com")
+
+with open("output.jpg","wb") as file:
+    file.write(img)
+
+# or
+snapshot("https://www.baidu.com", "baidu.jpg")
+```
+
+#### save as pdf
+
+```python
+from pyhtmlsnapshot import snapshot
+
+pdf = snapshot("https://www.baidu.com")
+
+snapshot("https://www.baidu.com", "baidu.pdf")
+```
+
+#### capture web page as pdf from url or file or content
+
+```python
+from pyhtmlsnapshot import snapshot
+
+# url
+snapshot("https://www.baidu.com", "baidu.pdf")
+
+# file
+snapshot("file:///some_static_web.html", "file.pdf")
+
+# string content
+html='''<!DOCTYPE html>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<html>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+    }
+</style>
+
+<body>
+    <h1> example
+    </h1>
+    <br>
+    <p>hello pyhtmlsnapshot</p>
+</body>
+
+</html>'''
+snapshot(html, "str.png")
+```
 
 # ref
 
--- pyhtmlsnapshot.py
+++ pyhtmlsnapshot.py
@@ -31,7 +31,7 @@ class AHTMLConverter:
         await self.init()
         return self
 
-    async def __aexit__(self, *args):
+    async def __aexit__(self):
         await self.finish()
 
     async def scrape_info(self, page, url, goto_options={}):
@@ -42,36 +42,37 @@ class AHTMLConverter:
         await page.goto(url, **goto_options)
         await page.evaluate('window.scrollBy(0, document.body.scrollHeight)')
 
-    async def _out_from_page(self, page, outfile=None, render_options={}):
+    async def _out_from_page(self, page, outfile, render_options={}):
         if outfile:
+            print(outfile)
             render_options["path"] = outfile
-            if outfile.endswith(".pdf"):
+            if str(outfile).endswith(".pdf"):
                 render_options = {
                     "format": "A4",
                 } | render_options
                 return await page.pdf(**render_options)
 
-            render_options = {
-                "fullPage": True
-            } | render_options
+        render_options = {
+            "fullPage": True
+        } | render_options
         return await page.screenshot(**render_options)
 
-    async def from_url(self, url: str, *args, goto_options={}, render_options={}):
+    async def from_url(self, url: str, outfile, goto_options={}, render_options={}):
         page = await self.browser.newPage()
         await self.scrape_info(page, url, goto_options)
-        out = await self._out_from_page(page, args, render_options)
+        out = await self._out_from_page(page, outfile, render_options)
         await page.close()
         return out
 
-    async def from_file(self, file_path: str, *args, goto_options={}, render_options={}):
+    async def from_file(self, file_path: str, outfile, goto_options={}, render_options={}):
         url = "file://" + realpath(file_path)
-        return await self.from_url(url, args, goto_options=goto_options, render_options=render_options)
+        return await self.from_url(url, outfile, goto_options, render_options)
 
-    async def from_string(self, content: str, *args, goto_options={}, render_options={}):
+    async def from_string(self, content: str, outfile, goto_options={}, render_options={}):
         with tempfile.NamedTemporaryFile(suffix=".html", delete=True) as f:
             f.write(content.encode())
             f.flush()
-            return await self.from_file(file_path=f.name, args=args, goto_options=goto_options, render_options=render_options)
+            return await self.from_file(f.name, outfile, goto_options, render_options)
 
 
 class HTMLConverter:
@@ -86,31 +87,33 @@ class HTMLConverter:
     def __del__(self):
         asyncio.get_event_loop().run_until_complete(self.converter.finish())
 
-    def from_url(self, url: str, *args, goto_options={}, render_options={}):
+    def from_url(self, url: str, outfile, goto_options={}, render_options={}):
         return asyncio.get_event_loop().run_until_complete(
-            self.converter.from_url(args, goto_options, render_options)
+            self.converter.from_url(url, outfile, goto_options, render_options)
         )
 
-    def from_file(self, file_path: str, *args, goto_options={}, render_options={}):
+    def from_file(self, file_path: str, outfile, goto_options={}, render_options={}):
         return asyncio.get_event_loop().run_until_complete(
-            self.converter.from_file(args, goto_options, render_options)
+            self.converter.from_file(
+                file_path, outfile, goto_options, render_options)
         )
 
-    def from_string(self, content: str, *args, goto_options={}, render_options={}):
+    def from_string(self, content: str, outfile, goto_options={}, render_options={}):
         return asyncio.get_event_loop().run_until_complete(
-            self.converter.from_string(args, goto_options, render_options)
+            self.converter.from_string(
+                content, outfile, goto_options, render_options)
         )
 
 
-def snapshot(origin: str, *args,
+def snapshot(origin: str, outfile=None,
              launch_options={},
              goto_options={},
              render_options={}):
     converter = HTMLConverter(launch_options)
     if origin.startswith("http"):
-        return converter.from_url(origin, args, goto_options, render_options)
+        return converter.from_url(origin, outfile, goto_options, render_options)
     if exists(origin):
-        return converter.from_file(origin, args, goto_options, render_options)
+        return converter.from_file(origin, outfile, goto_options, render_options)
     if origin != "":
         # origin = str(bs64decode(origin))
-        return converter.from_string(origin, args, goto_options, render_options)
+        return converter.from_string(origin, outfile, goto_options, render_options)
